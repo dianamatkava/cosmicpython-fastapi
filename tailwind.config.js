fa7a854b236926a714b55eab12a05ce7c8ad98e3
Diana <dianamatkava@gmail.com>
2023-04-13 22:19:13 +0200

    v1/templates: Adds header, mobile responcive main

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    theme: {
+      screens: {
+        'sm': '576px',
+        'md': '960px',
+        'lg': '1440px',
+      },
+    }
   },
   plugins: [],
 }

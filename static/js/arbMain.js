b188d2a64a4d2df794e9bc65843f5b6e1ca9d9b1
Diana <dianamatkava@gmail.com>
2023-04-23 14:02:30 +0200

    v1/template: Updates mob/desktop view

--- static/js/arbMain.js
+++ static/js/arbMain.js
@@ -18,7 +18,6 @@ function openForm() {
 function showDesc() {
     const form = document.getElementById('form');   
     const details = document.getElementById('details'); 
-    const showDesc = document.getElementById('showDesc'); 
     form.style.display = 'none'
     details.style.display = 'block'
 }
@@ -35,7 +34,6 @@ inputs.forEach(function(input) {
                 isFilled = false;
             }
         });
-        console.log(isFilled)
         if (isFilled) {
             submitButton.removeAttribute('disabled');
         } else {
@@ -72,8 +70,7 @@ function applyForm() {
             } else {
                 // All done!
                 document.getElementById('applyForm').style.display = 'block'
-            }
-            console.log(data)   
+            } 
         },
         error: function() {
             alert('Unelectable error occurred. Try later')
@@ -82,7 +79,48 @@ function applyForm() {
 };
 
 
-function changeLang(lang) {
-    console.log();
+const mediaQueryMin = window.matchMedia('(min-width: 1273px)');
+
+function mediaQueryMinFoo() {
+
+  const form = document.getElementById('form');
+  const desc = document.getElementById('desc');
+    // desc.appendChild(form) 
+  desc.insertBefore(form, desc.firstChild);
+
+
+  
 }
 
+mediaQueryMin.addEventListener('change', function(event) {
+  if (event.matches) {
+    mediaQueryMinFoo();
+  }
+});
+
+if (mediaQueryMin.matches) {
+    mediaQueryMinFoo();
+}
+
+
+
+const mediaQueryMax = window.matchMedia('(max-width: 1274px)');
+
+function myFunction() {
+    console.log('MOB');
+    const form = document.getElementById('form');
+    const mob = document.getElementById('mobForm');
+    const completed = document.getElementById('completed');
+    mob.appendChild(completed) 
+    mob.appendChild(form) 
+}
+
+mediaQueryMax.addEventListener('change', function(event) {
+  if (event.matches) {
+    myFunction();
+  }
+});
+
+if (mediaQueryMax.matches) {
+  myFunction();
+}

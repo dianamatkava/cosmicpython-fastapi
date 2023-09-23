fcd67996470b5fb1f5c621a2c495d749157e17b4
Diana <dianamatkava@gmail.com>
2023-09-23 10:00:40 +0200

    remove routes

--- arb/routes.py
+++ arb/routes.py
@@ -27,44 +27,44 @@ def validate_user_input(data: dict) -> Tuple[bool, dict]:
         return False, {'message': 'Email already exist'}
     return True, ''
 
-@arb.route('/.env', methods=['GET'])
-def hasker():
-    from flask.json import jsonify
-    return jsonify(**{'status': 'You are facking idiot', 'message': "stop hacking my site, i'm just a junior"})
+# @arb.route('/.env', methods=['GET'])
+# def hasker():
+#     from flask.json import jsonify
+#     return jsonify(**{'status': 'You are facking idiot', 'message': "stop hacking my site, i'm just a junior"})
 
 
-@arb.route('/', methods=['GET', 'POST'])
-def redirect_to_home():
-    if request.method == "POST":
+# @arb.route('/', methods=['GET', 'POST'])
+# def redirect_to_home():
+#     if request.method == "POST":
         
-        data = json.load(BytesIO(request.data))
-        status, res = validate_user_input(data)
-        if not status:
-            return jsonify(res)
-        try:
-            customer = CustomerData(**data)
-            db.session.add(customer)
-            db.session.commit()
-        except Exception as _ex:
-            return Response(status=400, response=_ex)
-        return Response(status=200)
-    return redirect(f'/{LANG}')
+#         data = json.load(BytesIO(request.data))
+#         status, res = validate_user_input(data)
+#         if not status:
+#             return jsonify(res)
+#         try:
+#             customer = CustomerData(**data)
+#             db.session.add(customer)
+#             db.session.commit()
+#         except Exception as _ex:
+#             return Response(status=400, response=_ex)
+#         return Response(status=200)
+#     return redirect(f'/{LANG}')
 
-@arb.route('/<lang>', methods=['GET'])
-def form(lang: str = LANG):
+# @arb.route('/<lang>', methods=['GET'])
+# def form(lang: str = LANG):
     
-    lang_map = {lang.value: lang for lang in Languages}
-    lang = lang_map.get(lang, False)
-    if not lang:
-        return redirect(url_for('arb.form', lang=LANG))
+#     lang_map = {lang.value: lang for lang in Languages}
+#     lang = lang_map.get(lang, False)
+#     if not lang:
+#         return redirect(url_for('arb.form', lang=LANG))
     
-    translations = Translation.query.filter_by(language_code=lang)
+#     translations = Translation.query.filter_by(language_code=lang)
     
-    context = {item.context_key: item.value for item in translations}
-    context['LANG'] = lang._value_
-    context['languages'] = lang_map.keys()
+#     context = {item.context_key: item.value for item in translations}
+#     context['LANG'] = lang._value_
+#     context['languages'] = lang_map.keys()
     
-    return render_template('arb-main-form.html', **context)
+#     return render_template('arb-main-form.html', **context)
 
 @arb.route('/mPxSaAeBfhR5ro', methods=['GET'])
 def WebHook():
